faa6706e0af006ef51051f20cd74900a4ac656e1
suraksha-niveus <103033871+suraksha-niveus@users.noreply.github.com>
2023-02-06 16:37:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ color_codes = {
 # color = os.environ.get('APP_COLOR') or random.choice(["red","green"])
 #color = os.environ.get('APP_COLOR') or random.choice(["white"])
 # color = os.environ.get('APP_COLOR') or random.choice(["pink","blue","yellow"])
-color = os.environ.get('APP_COLOR') or random.choice(["red"])
-# color = os.environ.get('APP_COLOR') or random.choice(["green"])
+# color = os.environ.get('APP_COLOR') or random.choice(["red"])
+color = os.environ.get('APP_COLOR') or random.choice(["green"])
 
 @app.route("/")
 def main():
